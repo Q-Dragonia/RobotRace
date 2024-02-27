16a7277c8647a711220b8a581d36697ae5da20d4
Pavel <pavels.rosickis@gmail.com>
2024-02-27 12:40:14 +0100

    Changed ports for movement
    Changed ports for direction pins of movement

--- definitions.h
+++ definitions.h
@@ -19,9 +19,9 @@
 #define SENSOR_LEFT_CHANNEL 0
 #define SENSOR_RIGHT_CHANNEL 3
 
-#define A_DIRECTION_PIN PB2 // Right motor direction pin
+#define A_DIRECTION_PIN PD2 // Right motor direction pin
 #define A_SPEED_PIN PD6     // Right motor speed pin
-#define B_DIRECTION_PIN PB4 // Left motor direction pin
+#define B_DIRECTION_PIN PD4 // Left motor direction pin
 #define B_SPEED_PIN PD5     // Left motor speed pin
 
 #define PWM_TOP 39999 // Period of the PWM signal for the servo (20ms)
